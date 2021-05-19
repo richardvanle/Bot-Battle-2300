fffac5c74475e7ce4113c0e976518bd82c217701
Richard V. Le <richardvanle@gmail.com>
2021-05-19 11:09:45 -0400

    Added more animations

---
+++ Assets/Scripts/botController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botController : MonoBehaviour
+{
+    public Animator botAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        botAnimator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            botAnimator.SetBool("isDead", true);
+            
+        }
+    }
+}
